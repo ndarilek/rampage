920754e0f905c658e4d8f8b1c39a8bc6273cbfa7
Nolan Darilek <nolan@thewordnerd.info>
2021-04-06 04:15:02 -0500

    Add actionmap plugin, and a system to greet.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use bevy::prelude::*;
+use bevy_input_actionmap::InputMap;
 use bevy_tts::Tts;
 
 #[bevy_main]
 fn main() {
     App::build()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(bevy_webgl2::DefaultPlugins)
+        .add_plugin(bevy_input_actionmap::ActionPlugin::<String>::default())
         .add_plugin(bevy_tts::TtsPlugin)
         .add_startup_system(setup.system())
+        .add_system(greet.system())
         .run();
 }
 
-fn setup(mut tts: ResMut<Tts>) {
-    tts.speak("Hello, world.", true).unwrap();
+const GREET: &str = "GREET";
+
+fn setup(mut input: ResMut<InputMap<String>>) {
+    input.bind(GREET, KeyCode::G);
+}
+
+fn greet(input: Res<InputMap<String>>, mut tts: ResMut<Tts>) {
+    if input.just_active(GREET) {
+        tts.speak("Hello, world.", true).unwrap();
+    }
 }
