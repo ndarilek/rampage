df13c8bf87edee8ee00a5d5463b962f65b73a51e
Nolan Darilek <nolan@thewordnerd.info>
2021-04-08 13:39:42 -0500

    Add better startup code and integrate OpenAL.

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,21 @@ use crate::error::error_handler;
 #[bevy_main]
 fn main() {
     App::build()
-        .add_plugins(DefaultPlugins)
         .add_plugin(crate::error::ErrorPlugin)
+        .insert_resource(WindowDescriptor {
+            title: "Rampage".into(),
+            ..Default::default()
+        })
+        .insert_resource(bevy::log::LogSettings {
+            level: bevy::log::Level::DEBUG,
+            // filter: "bevy_ecs=trace".into(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
         .add_plugin(bevy_input_actionmap::ActionPlugin::<String>::default())
+        .add_plugin(bevy_openal::OpenAlPlugin)
         .add_plugin(bevy_tts::TtsPlugin)
+        .add_system(bevy::input::system::exit_on_esc_system.system())
         .add_startup_system(setup.system())
         .add_system(greet.system().chain(error_handler.system()))
         .run();
